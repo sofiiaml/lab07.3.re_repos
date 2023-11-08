c0368161baec3894aaf4fee70eeadc95152853ae
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-11-08 21:50:16 +0200

    pr7.3.re

---
+++ PR7.3,re/PR7.3,re.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int** a, const int n,const int Low, const int High, int nIndex) {
+	if (nIndex == n)
+		return;
+	for (int i = 0; i < n; i++) {
+		a[nIndex][i] = Low + rand() % (High - Low + 1);
+	}
+	Create(a, n, Low, High, nIndex + 1);
+}
+void Print(int** a, const int n, int nIndex) {
+	if (nIndex == n)
+		return;
+	for (int j = 0; j < n; j++)
+		cout << setw(4) << a[nIndex][j];
+	cout << endl;
+	Print(a, n,nIndex + 1);
+}
+void Sort(int** a, int n, int k) {
+	if( k < n){
+		int max = INT_MIN;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (k == 0 && a[i][j] > max) {
+					max = a[i][j];
+				}
+				if (k > 0 && a[i][j] > max && a[i][j] < a[k - 1][k - 1]) {
+					max = a[i][j];
+				}
+			}
+		}
+		a[k][k] = max;
+		Sort(a, n, k + 1);
+	}
+}
+int findFirstRowWithoutPositive(int**a,int n, int i) {
+	if (i == n) {
+		return -1; 
+	}
+	bool hasPositive = false;
+	for (int j = 0; j < n; j++) {
+		if (a[i][j] > 0) {
+			hasPositive = true;
+			break;
+		}
+	}
+	if (!hasPositive) {
+		return i + 1; 
+	}
+	return findFirstRowWithoutPositive(a,n, i + 1);
+}
+int main() {
+	srand((unsigned)time(NULL));
+	int Low = -15;
+	int High = 5;
+	int n;
+	cout << "n = "; cin >> n;
+	int** a = new int* [n];
+	for (int i = 0; i < n; i++)
+		a[i] = new int[n];
+	Create(a, n, Low, High,0);
+	cout << "Array:"; cout << endl;
+	Print(a, n,0);
+	cout << "I=" << findFirstRowWithoutPositive(a, n, 0); cout << endl;
+	findFirstRowWithoutPositive(a, n,0);
+	Sort(a, n,0);
+	cout << "Changed Array:"; cout << endl;
+	Print(a, n,0);
+	for (int i = 0; i < n; i++) {
+		delete[] a[i];
+	}
+	delete[] a;
+	return 0;
+}
---
+++ UnitTest7.3.re/UnitTest7.3.re.cpp
@@ -0,0 +1,55 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR7.3,re/PR7.3,re.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest73re
+{
+	TEST_CLASS(UnitTest73re)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int** arr1 = new int* [3];
+			for (int i = 0; i < 3; i++) {
+				arr1[i] = new int[3];
+			}
+			arr1[0][0] = 4;
+			arr1[0][1] = 6;
+			arr1[0][2] = 33;
+			arr1[1][0] = 8;
+			arr1[1][1] = 30;
+			arr1[1][2] = 12;
+			arr1[2][0] = 24;
+			arr1[2][1] = 6;
+			arr1[2][2] = 7;
+			Sort(arr1, 3,0);
+			int** arr2 = new int* [3];
+			for (int i = 0; i < 3; i++) {
+				arr2[i] = new int[3];
+			}
+			arr2[0][0] = 33;
+			arr2[0][1] = 6;
+			arr2[0][2] = 33;
+			arr2[1][0] = 8;
+			arr2[1][1] = 30;
+			arr2[1][2] = 12;
+			arr2[2][0] = 24;
+			arr2[2][1] = 6;
+			arr2[2][2] = 24;
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					Assert::AreEqual(arr2[i][j], arr1[i][j]);
+				}
+			}
+
+			for (int i = 0; i < 3; i++) {
+				delete[] arr1[i];
+				delete[] arr2[i];
+			}
+			delete[]arr1, arr2;
+		}
+	};
+}
